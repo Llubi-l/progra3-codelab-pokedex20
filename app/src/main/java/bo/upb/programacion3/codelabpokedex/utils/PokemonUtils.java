e41a6a237278b7313af395f2b43bc6e814f2315a
Llubi-l <llubylinares@gmail.com>
2020-06-22 18:56:09 -0400

    22/6

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -26,6 +26,16 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13, "Blastoise", R.drawable.blastoise, "Water"));
+        pokemons.add(new Pokemon(14, "Clefairy", R.drawable.clefairy, "Fairy"));
+        pokemons.add(new Pokemon(15, "Eevee", R.drawable.eevee, "Normal"));
+        pokemons.add(new Pokemon(16,"Jigglypuff", R.drawable.jigglypuff, "Fairy"));
+        pokemons.add(new Pokemon(17, "Meowth", R.drawable.meowth, "Normal"));
+        pokemons.add(new Pokemon(18, "Mew", R.drawable.mew, "Psychic"));
+        pokemons.add(new Pokemon(19, "Mewtwo", R.drawable.mewtwo, "Psychic"));
+        pokemons.add(new Pokemon(20, "Onix", R.drawable.onix, "Ground"));
+        pokemons.add(new Pokemon(21, "Psyduck", R.drawable.psyduck, "Water"));
+        pokemons.add(new Pokemon(22, "Togepi", R.drawable.togepi,"Fairy"));
         return pokemons;
     }
 }
